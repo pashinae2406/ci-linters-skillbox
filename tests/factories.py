8b45ebf8063fa2da75d77679525297ccbe8af046
pashinae2406 <108214302+pashinae2406@users.noreply.github.com>
2025-07-14 17:09:11 +0300

    Update factories.py

--- tests/factories.py
+++ tests/factories.py
@@ -1,1 +1,29 @@
+import factory
+import factory.fuzzy as fuzzy
+import random
 
+from ..main import db
+from ..models import Client, Parking
+
+
+class ClientFactory(factory.alchemy.SQLAlchemyModelFactory):
+    class Meta:
+        model = Client
+        sqlalchemy_session = db.session
+
+    name = factory.Faker('first_name')
+    surname = factory.Faker('last_name')
+    credit_card = fuzzy.FuzzyChoice(["Есть карта", None])
+    car_number = fuzzy.FuzzyText(length=10)
+
+
+class ParkingFactory(factory.alchemy.SQLAlchemyModelFactory):
+    class Meta:
+        model = Parking
+        sqlalchemy_session = db.session
+
+    address = factory.Faker('address')
+    opened = fuzzy.FuzzyChoice([True, False])
+    count_places = fuzzy.FuzzyInteger(low=100, high=500)
+    count_available_places = factory.LazyAttribute(
+        lambda x: random.randrange(100, 500))
