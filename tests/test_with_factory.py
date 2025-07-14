be75646d47e0fe5efec11a0ed4c7fc2ea7ebd55b
pashinae2406 <108214302+pashinae2406@users.noreply.github.com>
2025-07-14 17:10:07 +0300

    Update test_with_factory.py

--- tests/test_with_factory.py
+++ tests/test_with_factory.py
@@ -1,1 +1,16 @@
+from .factories import ClientFactory, ParkingFactory
+from ..models import Client, Parking
 
+
+def test_create_client(app, db):
+    client_test = ClientFactory()
+    db.session.commit()
+    assert client_test.id is not None
+    assert len(db.session.query(Client).all()) == 2
+
+
+def test_create_parking(client, db):
+    parking = ParkingFactory()
+    db.session.commit()
+    assert parking.id is not None
+    assert len(db.session.query(Parking).all()) == 2
